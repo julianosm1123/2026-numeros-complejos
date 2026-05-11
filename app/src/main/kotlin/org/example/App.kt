5635fe71138cdedb367b090a78b71f691947955e
julianosm1123 <sanmartin.a.julian@gmail.com>
2026-05-11 09:06:37 -0300

    suma y resta añadidas

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -26,20 +26,41 @@ print(c.mostrar())  # Salida: (3, 4)
  */
 package org.example
 
-class Complejo{
-    private var real:Int=0
-    private var imaginario:Int=0
+class Complejo(private var real: Int=0,private var imag: Int=0) {
 
-    fun mostrarcomplejo(real:Int, imaginario:Int) {
+    fun inicializar(real:Int, imag:Int){
         this.real=real
-        this.imaginario=imaginario
-        println("El número es: (${real}, ${imaginario})")
+        this.imag=imag
+    }
+    fun mostrar(){
+        println(this.toString())
+    }
+    override fun toString() : String {
+        return " (${real}, ${imag})"
+    }
 
+    fun sumar(otro:Complejo): Complejo {
+        return Complejo(real + otro.real,imag + otro.imag)
+
+    }
+    fun restar(otro:Complejo): Complejo {
+        return Complejo(real-otro.real,imag-otro.imag)
     }
 
 }
+
+
+
 fun main() {
-    val complejo:Complejo
-    complejo=Complejo()
-    complejo.mostrarcomplejo(2, 5)
+    var complejo: Complejo
+    complejo = Complejo()
+    complejo.inicializar(3,4)
+    complejo.mostrar()
+    println(" Mi numero complejo es ${complejo.toString()}")
+    var c2:Complejo= Complejo(1,2)
+    var c3:Complejo
+    c3=complejo.sumar(c2)
+    c3.mostrar()
+    c3=complejo.restar(c2)
+    c3.mostrar()
 }
